12fe44297f3594e4d211f980b587a231b8d03714
Dimitri Lagast <lagast.dimitri@gmail.com>
2014-05-23 19:17:51 +0200

    - Added paths for minecart to follow.  - Added a base script to minecart that makes it follow the first path.

---
+++ PinballPlanet/Assets/Scenes/Mine/Scripts/MineCart.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineCart : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        iTweener newiTweener = new iTweener();
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("MineCart_StartPath"), "movetopath", false, "orienttopath", true, "speed", 150, "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.loop));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
