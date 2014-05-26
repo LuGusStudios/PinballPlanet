2a930a4d5200a353f5f92eca07a18a1d73bb36dd
Dimitri Lagast <lagast.dimitri@gmail.com>
2014-05-26 13:50:02 +0200

    - Mine cart now follows correct path depending on light switch.  - Added prefab for mine cart.  - Added script to paths/rails that handles mine carts.

--- PinballPlanet/Assets/Scenes/Mine/Scripts/MineCart.cs
+++ PinballPlanet/Assets/Scenes/Mine/Scripts/MineCart.cs
@@ -1,19 +1,111 @@
-﻿using UnityEngine;
+﻿using System.CodeDom;
+using System.Reflection.Emit;
+using UnityEngine;
 using System.Collections;
 
 public class MineCart : MonoBehaviour
 {
+    // iTween path names.
+    private const string StartPath =    "StartPath";
+    private const string DefaultPath =  "DefaultPath";
+    private const string BridgeStartPath = "BridgeStartPath";
+    private const string BridgeEndPath = "BridgeEndPath";
+
+    // Transform of upside down cart. Used to put cart upside down for weird iTween behaviour.
+    public Transform _upsideDownTransform;
 
     // Use this for initialization
     void Start()
     {
-        iTweener newiTweener = new iTweener();
-        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("MineCart_StartPath"), "movetopath", false, "orienttopath", true, "speed", 150, "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.loop));
+        _upsideDownTransform = transform.FindChild("MineCart02_UpsideDown").transform;
+
+        // Play start path animation.
+        StartPathMove();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    // Make cart follow start path.
+    void StartPathMove()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(StartPath)
+                                            , "movetopath", false
+                                            , "orienttopath", true
+                                            , "speed", 100
+                                            , "easetype", iTween.EaseType.linear
+                                            , "oncomplete", "OnStartPathEnded"));
+    }
+
+    // Called when the start path ends.
+    void OnStartPathEnded()
+    {
+        // Play default path animation if not following bridge path.
+        if (GameObject.Find("MineCart_Paths").GetComponent<MineCart_Rails>().RailsSwitched)
+            BridgeStartPathMove();
+        else
+            DefaultPathMove();
+    }
+
+    // Make cart follow start path.
+    void DefaultPathMove()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(DefaultPath)
+                                            , "movetopath", false
+                                            , "orienttopath", true
+                                            , "speed", 100
+                                            , "easetype", iTween.EaseType.linear
+                                            , "oncomplete", "OnDefaultPathEnded"));
+    }
+
+    // Called when the start path ends.
+    void OnDefaultPathEnded()
+    {
+        GameObject.Find("MineCart_Paths").GetComponent<MineCart_Rails>().OnMineCartDestroyed();
+        Destroy(gameObject);
+    }
+
+    // Make cart follow bridge start path.
+    void BridgeStartPathMove()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(BridgeStartPath)
+                                            , "movetopath", false
+                                            , "orienttopath", true
+                                            , "speed", 100
+                                            , "easetype", iTween.EaseType.linear
+                                            , "oncomplete", "OnBridgeStartPathEnded"));
+    }
+
+    // Called when the bridge start path ends.
+    void OnBridgeStartPathEnded()
+    {
+        // Play start path animation.
+        float delay = 2.0f;
+        Invoke("BridgeEndPathMove", delay);
+    }
+
+    // Make cart follow bridge path.
+    void BridgeEndPathMove()
+    {
+        // Set rotation upside down, to compensate for weird iTween behaviour.
+        transform.FindChild("MineCart02").rotation = _upsideDownTransform.rotation;
+
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(BridgeEndPath)
+                                            , "movetopath", false
+                                            , "orienttopath", true
+                                            , "looktime", 0.1f
+                                            , "speed", 100
+                                            , "easetype", iTween.EaseType.linear
+                                            , "oncomplete", "OnBridgeEndPathEnded"));
+    }
+
+    // Called when the bridge path ends.
+    void OnBridgeEndPathEnded()
+    {
+        GameObject.Find("MineCart_Paths").GetComponent<MineCart_Rails>().OnMineCartDestroyed();
+        Destroy(gameObject);
     }
 }
---
+++ PinballPlanet/Assets/Scenes/Mine/Scripts/MineCart_Rails.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Manages the mine cart and rails.
+/// </summary>
+public class MineCart_Rails : MonoBehaviour
+{
+    // Prefab of the mine cart.
+    public GameObject MineCartPrefab;
+
+    // Light that controls the rail switch.
+    public FloorLight_Toggle SwitchLight;
+
+    public bool RailsSwitched
+    {
+        get { return SwitchLight.IsBroken; }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        SpawnMineCart();
+    }
+
+    public void OnMineCartDestroyed()
+    {
+        SpawnMineCart();
+    }
+
+    private void SpawnMineCart()
+    {
+        Instantiate(MineCartPrefab);
+    }
+}
