027bb22984b6a3037e8cfb276c13a413cfc883f9
Dimitri Lagast <lagast.dimitri@gmail.com>
2014-05-28 11:31:03 +0200

    - Made TNT push animation faster.  - Added crystal grow animation when it resets.

--- PinballPlanet/Assets/Scenes/Mine/Scripts/Crystal.cs
+++ PinballPlanet/Assets/Scenes/Mine/Scripts/Crystal.cs
@@ -33,19 +33,25 @@ public class Crystal : Breakable
         switch (_breakIndex)
         {
             case 0:
+                // Disable crystal part.
                 _shard1.renderer.enabled = false;
                 ++_breakIndex;
                 break;
             case 1:
+                // Disable crystal part.
                 _shard2.renderer.enabled = false;
                 ++_breakIndex;
                 break;
             case 2:
+                // Disable crystal parts.
                 _shard3.renderer.enabled = false;
                 _particles.GetComponent<ParticleSystem>().enableEmission = false;
                 _light.GetComponent<Light>().enabled = false;
+                // Disable collider.
                 collider.enabled = false;
+                // Reset after a delay.
                 Invoke("Unbreak", ResetTime);
+                // Call base Break.
                 base.Break();
                 break;
         }
@@ -54,14 +60,21 @@ public class Crystal : Breakable
     // Restores the game object to its unbroken state.
     public override void Unbreak()
     {
+        // Reset index.
         _breakIndex = 0;
+        // Enable crystal parts.
         _shard1.renderer.enabled = true;
         _shard2.renderer.enabled = true;
         _shard3.renderer.enabled = true;
         _particles.GetComponent<ParticleSystem>().enableEmission = true;
         _light.GetComponent<Light>().enabled = true;
+        // Enable collision.
         collider.enabled = true;
 
+        // Play grow animation.
+        animation.Play("CrystalGrow");
+
+        // Call base Unbreak.
         base.Unbreak();
     }
 }
