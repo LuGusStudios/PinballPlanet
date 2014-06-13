3ae814bf0299bd89e4f684fd60f0f568708a45bf
Dimitri Lagast <lagast.dimitri@gmail.com>
2014-06-13 16:31:48 +0200

    - Changed launcher animation.

--- PinballPlanet/Assets/Scenes/Halloween/Scripts/HalloweenLauncher.cs
+++ PinballPlanet/Assets/Scenes/Halloween/Scripts/HalloweenLauncher.cs
@@ -16,6 +16,6 @@ public class HalloweenLauncher : MonoBehaviour
     void Update()
     {
         Vector3 scale = transform.localScale;
-		transform.localScale = new Vector3(scale.x, scale.y, Mathf.Lerp(_scale, 1, Player.use.BallLaunchForce / Player.use.LaunchMaxForce)); 
+		transform.localScale = new Vector3(scale.x, scale.y, Mathf.Lerp(_scale, 0.0285f, Player.use.BallLaunchForce / Player.use.LaunchMaxForce)); 
     }
 }
